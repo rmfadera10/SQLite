ea16cb4d93f68449277645cf0e1f77e3d024ef69
rmfadera10 <55060246+rmfadera10@users.noreply.github.com>
2019-12-05 18:03:23 +0545

    SQLite: - Insert data.

--- app/src/main/java/com/letstravel/sqlite/MainActivity.java
+++ app/src/main/java/com/letstravel/sqlite/MainActivity.java
@@ -2,10 +2,14 @@ package com.letstravel.sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import helper.MySQLHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,10 +24,21 @@ public class MainActivity extends AppCompatActivity {
         txtaddmeaning=findViewById(R.id.txtaddmeaning);
         btnAddwords=findViewById(R.id.btnaddword);
 
+
+        final MySQLHelper mySQLHelper=new MySQLHelper(MainActivity.this);
+        final SQLiteDatabase sqLiteDatabase = mySQLHelper.getWritableDatabase();
         btnAddwords.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+               long id = mySQLHelper.InsertData(txtaddword.getText().toString(),
+                       txtaddmeaning.getText().toString(), sqLiteDatabase);
+               if(id > 0){
+                   Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+               }
+               else{
+                   Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+               }
             }
         });
     }
--- app/src/main/java/helper/MySQLHelper.java
+++ app/src/main/java/helper/MySQLHelper.java
@@ -1,8 +1,10 @@
 package helper;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
@@ -11,12 +13,12 @@ public class MySQLHelper extends SQLiteOpenHelper {
     private static final String db_name="Dictionary";
     private static final int db_version=1;
 
-    private static final String tbl_name="tblword";
+    private static final String tbl_word="tblword";
 
 
     private static final String tbl_id="tblid";
-    private static final String wordName="wordName";
-    private static final String word_meaning="wordmeaning";
+    private static final String word="wordName";
+    private static final String meaning="wordmeaning";
 
 
     public MySQLHelper(@Nullable Context context) {
@@ -25,11 +27,23 @@ public class MySQLHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
-        String query = "CREATE TABLE " +tbl_name + "(" +tbl_id+ "INTEGER PRIMARY KEY AUTOINCREMENT ," +
-                wordName+ "TEXGT," +word_meaning+ "TEXT" + ")";
+        String query = "CREATE TABLE " + tbl_word  + "(" + tbl_id + " INTEGER PRIMARY KEY AUTOINCREMENT ," +
+                word + " TEXGT, " + meaning + " TEXT " + ")";
 
         db.execSQL(query);
+
+    }
+
+
+    public long InsertData(String wordName, String word_meaning, SQLiteDatabase db){
+
+        long id;
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(word, wordName);
+        contentValues.put(meaning, word_meaning);
+
+        id=db.insert(tbl_word, null, contentValues);
+        return id;
 
     }
 
